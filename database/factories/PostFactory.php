95f37636f413979427428125e8a6feb8db3c1b28
Francisco Corona <franciscoj.coronal@gmail.com>
2022-10-19 20:49:52 -0500

    Relationshiops test

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,6 +19,7 @@ class PostFactory extends Factory
     {
         return [
             //
+            'user_id' => 1,
             'title' => $title = $this->faker->sentence(),
             'slug'  => Str::slug($title),
             'body'  => $this->faker->text(2200),
