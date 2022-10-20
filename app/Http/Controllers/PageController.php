300c712a4dbef82cc0dc42a265a05c8da7419dd5
Francisco Corona <franciscoj.coronal@gmail.com>
2022-10-19 20:33:28 -0500

    Prueba de eloquent

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -16,18 +17,15 @@ class PageController extends Controller
 
     public function blog()
     {
-        $posts = [
-            ['id' => 1, 'title' => 'PHP',       'slug' => 'php' ],
-            ['id' => 2, 'title' => 'Laravel',   'slug' => 'Laravel' ]
-        ];
+        $posts = Post::latest()->paginate();
     
         return view ('blog', ['posts' => $posts]);
     }
 
 
-    public function post($slug)
+    public function post(Post $post)
     {
-        $post = $slug;
+
 
         return view('post', ['post' => $post]);
     }
