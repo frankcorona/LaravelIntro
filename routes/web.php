b78b1806f06b69021ba8d721999de82735006e16
Francisco Corona <franciscoj.coronal@gmail.com>
2022-10-20 21:22:18 -0500

    Prueba Login

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,17 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::controller(PageController::class)->group(function (){
 
     Route::get('/',               'home')->name('home');
     Route::get('blog',            'blog')->name('blog');
     Route::get('blog/{post:slug}',     'post')->name('post');
+
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
